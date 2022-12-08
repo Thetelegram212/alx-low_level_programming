96e059100d3f3bc7c7695c7baf088880a5d9f45b
Adekoya Dotun <adekoyateleola@gmail.com>
2022-12-08 08:37:54 -0800

    Hello World Task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+ /**
+   * main - prints line with proper grammar,
+   * but the outcome is a pieace of art,
+   * Return: 0
+   */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
