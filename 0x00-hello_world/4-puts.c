76bb944a85a770412fff6b2b5ba84dd99eb293c9
Adekoya Dotun <adekoyateleola@gmail.com>
2022-12-08 08:56:44 -0800

    fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-   *main - prints  line "Programming is like building a multilingual puzzle
-    *Return: 0
-    */
-  int main(void)
- {
-	 puts("\"Programming is like building a multilingual puzle");
-	 return(0);
- }
+   * main - prints  line "Programming is like building a multilingual puzzle
+   * Return: 0
+   */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzle");
+	return (0);
+}
